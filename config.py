fcd172fa51ce7f31baf68bb6aaa2326f5c531d20
Marika Lee <marika.lee@rakuten.com>
2016-10-23 08:43:33 -0700

    change keys

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-public_key = "4cdee87aba77450caa2ca5c545591662"
-private_key = "8XHOysAA2OEC0ODYufQHsSyBQjXfCK"
+public_key = "afeb8798470e420d8c52568c9ff666a2"
+private_key = "JQhH39YW2mrwLxJHE2qx44fT5bBc4e"
